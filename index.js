02260247905bd4d844234d872f1c384a676859e3
sureshasoori <sureshasoori60@gmail.com>
2021-11-07 16:39:45 +0530

    nov3/1

---
+++ index.js
@@ -0,0 +1,14 @@
+/*let arr=[1,2,3,4,5,5];
+arr.forEach((elemen,indext) => {
+    console.log(element)
+    console.log(index)
+})
+let obj={
+    name:'suresh'
+    age:22
+}
+for(x in obj){
+    console.log(x)
+    console.log()
+}*/
+console.log("i am suresha");
